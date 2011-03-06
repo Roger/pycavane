aa037c8a373cd87ef2df54e4aa3a37deed935708
roger <roger@rogerpc.com.ar>
2011-03-06 13:46:38 -0300

    start support of movies

--- pycavane.py
+++ pycavane.py
@@ -10,12 +10,16 @@ headers = {'User-Agent': 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; '\
 
 host = 'http://www.cuevana.tv'
 
+movies_url = host+'/list_peliculas.php?list=2&'+host+\
+                    '/peliculas/lista/letra=%s&page=%s'
+
 series_url = host+'/series/'
 seassons_url = host+'/list_search_id.php?serie=%s'
 episodes_url = host+'/list_search_id.php?temporada=%s'
 
 episode_url = host+"/series/%s/%s/%s/"
-player_url = host+'/player/source?id=%s&subs=,ES,EN&onstart=yes&tipo=s&sub_pre=ES'
+player_movie_url = host+'/player/source?id=%s&subs=,ES,EN&onstart=yes&sub_pre=ES'
+player_season_url = player_movie_url+'&tipo=s'
 source_get = host+'/player/source_get'
 
 sub_url = host+'/files/s/sub/%s_%s.srt'
@@ -25,6 +29,11 @@ series_re = re.compile('serieslist.push\(\{id:([0-9]*),nombre:"(.*?)"\}\);')
 seasson_re = re.compile('<li onclick=\'listSeries\(2,"([0-9]*)"\)\'>(.*?)</li>')
 episode_re = re.compile('<li onclick=\'listSeries\(3,"([0-9]*)"\)\'>'\
                         '<span class=\'nume\'>([0-9]*)</span>(.*?)</li>')
+
+movies_re = re.compile(r'<tr class=\'row[1-2]\'>.*?<div class=\'tit\'><a '\
+              'href=\'(/peliculas/[0-9]*?/.*?/)\'>(.*?)</a></div>.*?<div '\
+              'class=\'font11\'>(.*?)<div class=\'reparto\'>', re.DOTALL)
+
 mega_id_re = re.compile('goSource\((.*?)\',\'megaupload\'\)')
 
 captcha_re = re.compile('<img src="(http\:\/\/.*megaupload\.com\/'\
@@ -86,6 +95,17 @@ def url_open(url, data=None, filename=None, handle=False):
 
     return text
 
+def get_movies(letter='num', page=0):
+    all_movies = []
+    while True:
+        page += 1
+        page_data = url_open(movies_url%(letter, page))
+        moov = movies_re.findall(page_data)
+        if not moov:
+            break
+        all_movies += moov
+    return all_movies
+
 def get_series(name=None):
     series = series_re.findall(url_open(series_url))
     if name:
@@ -107,8 +127,12 @@ def get_megalink(link):
         return megalink[0]
     return None
 
-def get_direct_links(episode, host=None):
-    data = url_open(player_url % episode[0])
+def get_direct_links(episode, host=None, movie=False):
+    if movie:
+        url = player_movie_url
+    else:
+        url = player_season_url
+    data = url_open(url % episode[0])
     hosts = []
     for key, value in source_re.findall(data):
         if not host or value == host:
