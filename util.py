d68808c8ee35e8399a168cfe1034596b095f94d6
Roger Duran <roger@elvex.org.ar>
2011-05-07 17:18:18 -0300

    added cookies support in url_open

--- util.py
+++ util.py
@@ -1,6 +1,8 @@
+import sys
 import time
 import urllib
 import urllib2
+import cookielib
 
 class Singleton(object):
     '''Not really singleton but close
@@ -22,49 +24,64 @@ headers = {'User-Agent': 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; '\
 
 def retry(callback):
     ''' Retry decorator '''
-    def deco(url, data=None, filename=None, handle=False):
+    def deco(url, *args, **kwargs):
         tried = 0
-        while tried < 30:
+        while tried < 10:
             try:
-                return callback(url, data, filename, handle)
+                return callback(*args, **kwargs)
             except Exception, error:
                 tried += 1
                 time.sleep(1)
-        raise Exception, 'Can\'t download "%s" error: "%s"' % (url, error)
+        error = 'Can\'t download\nerror: "%s"\n args: %s' % \
+                            (error, str(args) + str(kwargs))
+        raise Exception, error
     return deco
 
-@retry
-def url_open(url, data=None, filename=None, handle=False):
-    if data:
-        request = urllib2.Request(url, urllib.urlencode(data), headers)
-    else:
-        request = urllib2.Request(url, headers=headers)
-    rc = urllib2.urlopen(request)
 
-    if handle:
-        return rc
+class UrlOpener(object):
+    ''' An url opener with cookies support '''
+    def __init__(self):
+        self.setup_cookies()
 
-    if filename:
-        local = open(filename, 'wb')
+    @retry
+    def __call__(url, data=None, filename=None, handle=False):
+        if data:
+            request = urllib2.Request(url, urllib.urlencode(data), headers)
+        else:
+            request = urllib2.Request(url, headers=headers)
+        rc = urllib2.urlopen(request)
 
-    text = ''
-    size = 0
-    lastsize = 0
-
-    while True:
-        buffer = rc.read(1024)
-        if buffer == '':
-            break
+        if handle:
+            return rc
 
         if filename:
-            size += len(buffer)
-            local.write(buffer)
-            if (size - 1024*4) > lastsize:
-                sys.stdout.write(str(size/1024) + "kb downloaded...\r")
-                sys.stdout.flush()
-                lastsize = size
-        else:
-            text += buffer
-    if filename:
-        local.close()
-    return text
+            local = open(filename, 'wb')
+
+        text = ''
+        size = 0
+        lastsize = 0
+
+        while True:
+            buffer = rc.read(1024)
+            if buffer == '':
+                break
+
+            if filename:
+                size += len(buffer)
+                local.write(buffer)
+                if (size - 1024*4) > lastsize:
+                    sys.stdout.write(str(size/1024) + "kb downloaded...\r")
+                    sys.stdout.flush()
+                    lastsize = size
+            else:
+                text += buffer
+        if filename:
+            local.close()
+        return text
+
+    def setup_cookies(self):
+        ''' Setup cookies in urllib2 '''
+        jar = cookielib.CookieJar()
+        handler = urllib2.HTTPCookieProcessor(jar)
+        opener = urllib2.build_opener(handler)
+        urllib2.install_opener(opener)
