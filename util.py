dbf5d3b89e87949cff813bb250c1e6a978a743b2
Roger Duran <roger@elvex.org.ar>
2011-05-26 01:25:01 -0300

    removed Singleton, dont needed in pycavane

--- util.py
+++ util.py
@@ -16,23 +16,6 @@ HEADERS = {
 RETRY_TIMES = 5
 
 
-class Singleton(object):
-    """
-    Not really singleton but close taken from dx
-    https://github.com/dequis/derpbot/blob/master/util.py
-    """
-    instance = None
-
-    def __init__(self):
-        if type(self).instance is not None:
-            raise TypeError("Already instantiated, use .get()")
-        type(self).instance = self
-
-    @classmethod
-    def get(cls):
-        return cls.instance or cls()
-
-
 def retry(callback):
     """
     Retry decorator.
